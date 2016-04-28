4d1a211411beb7856a0d4d175d64d59ff2c2f635
Ilya Kulebyakin <ilya.kulebyakin@e-legion.com>
2016-04-28 13:50:59 +0300

    Setup Framework project

---
+++ Source/ELNTestUtils.h
@@ -0,0 +1,19 @@
+//
+//  ELNTestUtils.h
+//  ELNTestUtils
+//
+//  Created by Ilya Kulebyakin on 28/04/16.
+//  Copyright © 2016 e-legion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ELNTestUtils.
+FOUNDATION_EXPORT double ELNTestUtilsVersionNumber;
+
+//! Project version string for ELNTestUtils.
+FOUNDATION_EXPORT const unsigned char ELNTestUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ELNTestUtils/PublicHeader.h>
+
+#import <ELNTestUtils/XCTestCase+ELNHelpers.h>
